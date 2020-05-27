b4767e1a096761f7a1654c2831ca28c1bc618c17
dev-fukushima <w9tow9@gmail.com>
2020-05-28 00:25:55 +0900

    CSV連続置換機能追加

--- make.php
+++ make.php
@@ -4,6 +4,8 @@ include_once dirname(__FILE__).'/' .'class.csv_update_module.inc';
 ini_set('display_errors', "On");
 CSV_UPDATE_MODULE::set_csv_filename('create');
 CSV_UPDATE_MODULE::force_load_csv();
-CSV_UPDATE_MODULE::set_template_html(dirname(__FILE__).'/' .'template.html');
-CSV_UPDATE_MODULE::force_create_html();
+//CSV_UPDATE_MODULE::set_template_html(dirname(__FILE__).'/' .'template.html');
+CSV_UPDATE_MODULE::set_template_html(dirname(__FILE__).'/' .'replace.html');
+//CSV_UPDATE_MODULE::force_create_html();
+CSV_UPDATE_MODULE::replace_html();
 ?>
